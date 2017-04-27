6855e82b2a27590becbab5d74f4faeeed1ba6db3
maitc <maibeet.torres@gmail.com>
2017-04-27 10:48:33 -0400

    boton ok

---
+++ assets/js/javascript.js
@@ -0,0 +1,85 @@
+var lista = [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
+  }];
+
+
+/*function reducirDiez(lista){
+	var arr = [];
+	for(var i = 1; i <= 10; i++){
+		arr += i;
+	}
+	return arr;
+}
+
+document.write(reducirDiez(lista));*/
+
+/*function creacionLista(){
+	var llamarList = document.getElementsById("1");
+	llamarList.innerHTML = lista;
+}*/
+
+function aniadir(){
+	var input = document.getElementById("inputAñadir").value;
+	var mostrarList = document.getElementById("mostrar");
+	mostrarList.innerHTML = input;
+}
+
+
